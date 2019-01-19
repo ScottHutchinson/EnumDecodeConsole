2c279efc777f2cd5f427537e4e3fe7e502f3f9a1
Scott Hutchinson <scotthutchinson@usa.net>
2019-01-19 08:15:45 -0800

    Replaced function pointers with nullptr.

--- EnumDecodeConsole/Script.fsx
+++ EnumDecodeConsole/Script.fsx
@@ -8,16 +8,21 @@ let listToStringBuilder (sb : StringBuilder) f lst =
     sb.Clear() |> ignore
     lst |> List.iter (fun x -> appendIgnore sb (f x))
 
-
-let fmt:Printf.StringFormat<_> = @"const string sDecode_ACTIVATION_TYPE%d(const int /*idx*/) { return ""x""; }"
-let intToString i =
-    sprintf fmt i
-
 let sb = StringBuilder()
 let n = 2000
 
-let addFunctions sb =
-    List.init n intToString
+let replicate sb f =
+    List.init n f
     |> listToStringBuilder sb (fun x -> sprintf "%s\r\n" x)
 
-addFunctions sb
+let intToString fmt i =
+    sprintf fmt i
+
+//let fmt:Printf.StringFormat<_> = @"const string sDecode_ACTIVATION_TYPE%d(const int /*idx*/) { return ""x""; }"
+//replicate sb (intToString fmt)
+
+//let intToString' fmt i =
+//    sprintf fmt i i
+
+let fmt':Printf.StringFormat<_> = @"        { ""ACTIVATION_TYPE%d"", nullptr },"
+replicate sb (intToString fmt')
